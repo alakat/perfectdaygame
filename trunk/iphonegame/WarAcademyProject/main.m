6591186d032ff3104c6d85746fad9e06ef67d54a
alakat@gmail.com <alakat@gmail.com@0f029474-5f5b-11de-b3c1-0fd540ce45e4>
2010-09-01 14:20:31 +0000

    commit initical

---
+++ trunk/iphonegame/WarAcademyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WarAcademyProject
+//
+//  Created by Miguel Angel  Lopez Montellano on 31/08/10.
+//  Copyright BitsOnFire Spanish iPhone Game Desing 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"WarAcademyProjectAppDelegate");
+	[pool release];
+	return retVal;
+}
