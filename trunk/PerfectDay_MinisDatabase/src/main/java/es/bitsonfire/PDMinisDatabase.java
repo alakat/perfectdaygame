1a19dde9a0b9ae73131269e9f17aad1bcff22259
alakat <alakat@0f029474-5f5b-11de-b3c1-0fd540ce45e4>
2015-08-07 21:18:04 +0000

    movida la carpeta de MinisDatabase a nivel inferior

---
+++ trunk/PerfectDay_MinisDatabase/src/main/java/es/bitsonfire/PDMinisDatabase.java
@@ -0,0 +1,13 @@
+package es.bitsonfire;
+
+/**
+ * Hello world!
+ *
+ */
+public class PDMinisDatabase 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Nothing for do" );
+    }
+}
