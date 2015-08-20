18c1dbf79a025ef84976793f45b58dcf3ece6cda
alakat <alakat@0f029474-5f5b-11de-b3c1-0fd540ce45e4>
2015-08-20 18:09:14 +0000

    Movimiento a Git

---
+++ PerfectDay_Client/src/main/java/org/perfectday/main/dummyengine/commons/Sprite.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.perfectday.main.dummyengine.commons;
+
+/**
+ *
+ * @author Miguel (alakat@gmail.com)
+ */
+public class Sprite {
+    
+}
